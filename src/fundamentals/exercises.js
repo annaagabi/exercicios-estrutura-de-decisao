5b29602a6350a63741f789c5c48d85a4bd7df666
annaagabi <annaa0478@gmail.com>
2024-08-31 11:35:02 -0300

    Feat: explicação e exercicios sobre if e else

--- src/fundamentals/exercises.js
+++ src/fundamentals/exercises.js
@@ -1,4 +1,5 @@
 // Explicação Ternário
+// O ternário é usado quando há apenas duas condições
 
 function validaIdade(idade){
     return (
@@ -36,4 +37,77 @@ function cliente(clienteFidelidade){
         clienteFidelidade === true ? console.log("10% de desconto") : ("5% de desconto")
     )
 }
-cliente(true)
+// cliente(true)
+
+
+/* Explicação if e else */
+
+// O if é usado quando há apenas quatro condições
+
+function validaIdadeIf(idade){
+    if(idade>=18){
+        return console.log("Maior de idade")
+
+    } else{
+        return console.log("Menor de idade")
+    }
+}
+
+// validaIdadeIf(17)
+
+function validaNota(nota){
+
+    if(nota>=7){
+        console.log("Aprovado")
+
+    } else if(nota>=5 && nota<=6){
+        return console.log("Recuperação")
+    }else{
+        console.log("Reprovado")
+    }
+
+}
+
+// validaNota(3)
+
+
+/* 2. Exercícios de Funções com `if` e `else` */
+
+/*Exercício 1: Avaliação de Notas
+
+Crie uma função que receba uma nota como parâmetro e retorne "Aprovado" se a nota for maior ou igual a 60, e "Reprovado" caso contrário.
+*/
+
+function validaNota2(nota){
+
+    if(nota>=60){
+        console.log("Aprovado")
+
+    } else{
+        console.log("Reprovado")
+    }
+
+}
+
+// validaNota(62)
+
+
+/* Exercício 2: Verificação de Número Positivo/Negativo
+
+Crie uma função que receba um número e retorne "Positivo", "Negativo" ou "Zero".*/
+
+function verificaNum(num){
+    if(num==0){
+        return console.log("Zero")
+
+    } else if(num>0){
+        return console.log("Positivo")
+
+    } else{
+        return console.log("Negativo")
+    }
+}
+
+verificaNum(0)
+verificaNum(4)
+verificaNum(-9)
